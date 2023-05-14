3b1e9e909866abb8ac033b0b0d06f22c2b19bf82
dmitryfedorov228 <116007725+dmitryfedorov228@users.noreply.github.com>
2023-05-14 15:46:15 +0600

    add allvfiles

--- main.js
+++ main.js
@@ -19,6 +19,7 @@ const addBtn = document.querySelector('.form_btn')
 addBtn.addEventListener('click', (event) => {
   event.preventDefault()
   ShowForm()
+
 })
 
 function ShowForm() {
@@ -28,15 +29,33 @@ function ShowForm() {
   const phone = phoneInput.value;
 
   const form = {
-    name,
-    lastName,
-    email,
-    phone
+    name : name,
+    lastName : lastName,
+    email : email, 
+    phone : phone
   };
+
+  
+ 
+ 
+
+
+
+  if(name === ''.trim()) {
+    alert('name is empty')
+  } else if (lastName === ''.trim()) {
+    alert('last name is empty')
+  } else if(email === ''.trim()) {
+    alert('email is empty')
+  } else if (phone === ''.trim()) {
+    alert('phone is empty'),
+    phone = 'empty'
+
+  }
 
   const formList = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
   formList.push(form);
   localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(formList));
 
-  console.log(`Имя: ${name} Фамилия: ${lastName} Почта: ${email} Номер: ${phone}`);
+  console.log(`Имя: ${name}, Фамилия: ${lastName}, Почта: ${email}, Номер: ${phone}.`);
 }
